a41fd2bd5582c0a65db4cb860be1991f7cd5cfe9
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-05-30 11:06:41 +0800

    Update datamgr.py

--- data/datamgr.py
+++ data/datamgr.py
@@ -6,7 +6,6 @@ import numpy as np
 import torchvision.transforms as transforms
 from torchvision.transforms import AutoAugment, AutoAugmentPolicy, InterpolationMode, RandAugment, AugMix
 import data.additional_transforms as add_transforms
-from data.stainnet_transform import StainNetTransform
 from data.dataset import SimpleDataset, SetDataset, EpisodicBatchSampler
 from abc import abstractmethod
 import os
@@ -25,36 +24,6 @@ class TransformLoader:
             method = add_transforms.ImageJitter( self.jitter_param )
             return method
 
-        elif transform_type=='StainNetTransform':
-            method = StainNetTransform()
-            return method
-
-        elif transform_type == 'AutoAugment':
-            policy = AutoAugmentPolicy.IMAGENET  # You can change this to another policy if needed
-            interpolation = InterpolationMode.BILINEAR
-            fill = None
-            method = AutoAugment(policy, interpolation, fill)
-            return method
-
-        elif transform_type == 'RandAugment':
-            num_ops = 2  # Number of augmentation transformations to apply sequentially
-            magnitude = 9  # Magnitude for all the transformations
-            num_magnitude_bins = 31  # The number of different magnitude values
-            interpolation = InterpolationMode.BILINEAR
-            fill = None
-            method = RandAugment(num_ops, magnitude, num_magnitude_bins, interpolation, fill)
-            return method
-
-        elif transform_type == 'AugMix':
-            severity = 3  # The severity of base augmentation operators
-            mixture_width = 3  # The number of augmentation chains
-            chain_depth = -1  # The depth of augmentation chains
-            alpha = 1.0  # The hyperparameter for the probability distributions
-            all_ops = True  # Use all operations (including brightness, contrast, color, and sharpness)
-            interpolation = InterpolationMode.BILINEAR
-            fill = None
-            method = AugMix(severity, mixture_width, chain_depth, alpha, all_ops, interpolation, fill)
-            return method
             
         method = getattr(transforms, transform_type)
         if transform_type=='RandomResizedCrop':
@@ -70,24 +39,8 @@ class TransformLoader:
             return method()
 
     def get_composed_transform(self, aug=None, sn=False):
-        if aug == 'standard' and sn:
-            transform_list = ['RandomResizedCrop', 'StainNetTransform', 'ImageJitter', 'RandomVerticalFlip', 'RandomHorizontalFlip', 'ToTensor', 'Normalize']
-        elif aug == 'standard':
+        if aug == 'standard':
             transform_list = ['RandomResizedCrop', 'ImageJitter', 'RandomVerticalFlip', 'RandomHorizontalFlip', 'ToTensor', 'Normalize']
-        elif aug == 'auto' and sn:
-            transform_list = ['Resize', 'CenterCrop', 'AutoAugment', 'StainNetTransform', 'ToTensor', 'Normalize']
-        elif aug == 'auto':
-            transform_list = ['Resize', 'CenterCrop', 'AutoAugment', 'ToTensor', 'Normalize']
-        elif aug == 'rand' and sn:
-            transform_list = ['Resize', 'CenterCrop', 'RandAugment', 'StainNetTransform', 'ToTensor', 'Normalize']
-        elif aug == 'rand':
-            transform_list = ['Resize', 'CenterCrop', 'RandAugment', 'ToTensor', 'Normalize']
-        elif aug == 'augmix' and sn:
-            transform_list = ['Resize', 'CenterCrop', 'AugMix', 'StainNetTransform', 'ToTensor', 'Normalize']
-        elif aug == 'augmix':
-            transform_list = ['Resize', 'CenterCrop', 'AugMix', 'ToTensor', 'Normalize']
-        elif aug == 'none' and sn:
-            transform_list = ['Resize', 'CenterCrop', 'StainNetTransform', 'ToTensor', 'Normalize']
         elif aug == 'none':
             transform_list = ['Resize', 'CenterCrop', 'ToTensor', 'Normalize']
         else:
@@ -113,10 +66,10 @@ class SimpleDataManager(DataManager):
         self.trans_loader = TransformLoader(image_size)
 
     
-    def get_data_loader(self, data_file, aug, sn): #parameters that would change on train/val set
+    def get_data_loader(self, data_file, aug): #parameters that would change on train/val set
         
 
-        transform = self.trans_loader.get_composed_transform(aug = aug, sn=sn)
+        transform = self.trans_loader.get_composed_transform(aug = aug)
         dataset = SimpleDataset(data_file, transform = transform)
 
         data_loader_params = dict(batch_size = self.batch_size, shuffle = True, num_workers = os.cpu_count(), pin_memory = True) 
@@ -135,10 +88,10 @@ class SetDataManager(DataManager):
 
         self.trans_loader = TransformLoader(image_size)
 
-    def get_data_loader(self, data_file, aug, sn, cutmix = False, mixup = False): #parameters that would change on train/val set
+    def get_data_loader(self, data_file, aug): #parameters that would change on train/val set
         
 
-        transform = self.trans_loader.get_composed_transform(aug = aug, sn=sn)
+        transform = self.trans_loader.get_composed_transform(aug = aug)
         dataset = SetDataset( data_file , self.batch_size, transform = transform)
         sampler = EpisodicBatchSampler(len(dataset), self.n_way, self.n_eposide )  
 
