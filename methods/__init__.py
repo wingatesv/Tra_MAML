8dbe8c1b89aafaedc149e6e5dbe922787901eb3a
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-05-30 11:29:50 +0800

    Update __init__.py

--- methods/__init__.py
+++ methods/__init__.py
@@ -5,8 +5,7 @@ from . import matchingnet
 from . import protonet
 from . import relationnet
 from . import maml
-from . import anil
-from . import anneal_maml
-from . import xmaml
-from . import tra_anil
+from . import tra_maml
+
+
 
