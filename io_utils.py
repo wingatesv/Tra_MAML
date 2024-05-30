bdce73fdc8513cc43d212a5cdbc0809b1b1ad327
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-05-30 11:59:29 +0800

    Update io_utils.py

--- io_utils.py
+++ io_utils.py
@@ -19,16 +19,14 @@ model_dict = dict(
 
 def parse_args(script):
     parser = argparse.ArgumentParser(description= 'few-shot script %s' %(script))
-    parser.add_argument('--dataset'     , default='BreaKHis_4x',        help='BreaKHis_4x, BreaKHis_10x, BreaKHis_20x, BreaKHis_40x, ISIC, Smear')
+    parser.add_argument('--dataset'     , default='BreaKHis_40x',        help='BreaKHis_40x, ISIC, Smear, cross_IDC')
     parser.add_argument('--model'       , default='ResNet10',      help='model: Conv{4|6} / ResNet{10|18|34|50|101}') # 50 and 101 are not used in the paper
-    parser.add_argument('--method'      , default='baseline',   help='baseline/baseline++/protonet/matchingnet/relationnet{_softmax}/maml{_approx}/anil/annemaml') #relationnet_softmax replace L2 norm with softmax to expedite training, maml_approx use first-order approximation in the gradient for efficiency
+    parser.add_argument('--method'      , default='baseline',   help='baseline/baseline++/protonet/matchingnet/relationnet{_softmax}/maml{_approx}/tra_maml') #relationnet_softmax replace L2 norm with softmax to expedite training, maml_approx use first-order approximation in the gradient for efficiency
     parser.add_argument('--train_n_way' , default=3, type=int,  help='class num to classify for training') #baseline and baseline++ would ignore this parameter
     parser.add_argument('--test_n_way'  , default=3, type=int,  help='class num to classify for testing (validation) ') #baseline and baseline++ only use this parameter in finetuning
     parser.add_argument('--n_shot'      , default=1, type=int,  help='number of labeled data in each class, same as n_support') #baseline and baseline++ only use this parameter in finetuning
-    parser.add_argument('--train_aug'   , default='none', type=str, help='perform data augmentation or not during training, aug: none, standard, auto, rand, augmix') #still required for save_features.py and test.py to find the model path correctly
-    parser.add_argument('--sn'   , action='store_true', help='Apply StainNet Stain Normalization')
-    parser.add_argument('--anneal_param'   , default='none', type=str, help='Annealing parameters')
-
+    parser.add_argument('--train_aug'   , default='none', type=str, help='perform data augmentation or not during training, aug: none, standard') #still required for save_features.py and test.py to find the model path correctly
+    parser.add_argument('--tra'   , default='none', type=str, help='TRA configurations')
 
 
     if script == 'train':
@@ -47,14 +45,6 @@ def parse_args(script):
         parser.add_argument('--split'       , default='novel', help='base/val/novel') #default novel, but you can also test base/val class accuracy if you want 
         parser.add_argument('--save_iter', default=-1, type=int,help ='saved feature from the model trained in x epoch, use the best model if x is -1')
         parser.add_argument('--adaptation'  , action='store_true', help='further adaptation in test time or not')
-
-    elif script == 'grad_cam':
-        parser.add_argument('--method_1'      , default='maml',   help='baseline/baseline++/protonet/matchingnet/relationnet{_softmax}/maml{_approx}/anil/imaml_idcg') 
-        parser.add_argument('--method_2'      , default='anil',   help='baseline/baseline++/protonet/matchingnet/relationnet{_softmax}/maml{_approx}/anil/imaml_idcg') 
-        parser.add_argument('--model_1'       , default='ResNet34',      help='model: Conv{4|6} / ResNet{10|18|34|50|101}')
-        parser.add_argument('--model_2'       , default='ResNet34_IM_F',      help='model: Conv{4|6} / ResNet{10|18|34|50|101}')
-        parser.add_argument('--split'       , default='novel', help='base/val/novel') #default novel, but you can also test base/val class accuracy if you want 
-        parser.add_argument('--save_iter', default=-1, type=int,help ='saved feature from the model trained in x epoch, use the best model if x is -1')
 
     else:
        raise ValueError('Unknown script')
