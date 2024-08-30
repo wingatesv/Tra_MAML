4ad6ed8fe841c5f2b0709da19c0b3e6ffe3ba9a4
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-08-30 12:23:14 +0800

    Update test.py

--- test.py
+++ test.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
         else:
             modelfile   = get_best_file(checkpoint_dir)
         if modelfile is not None:
-            tmp = torch.load(modelfile)
+            tmp = torch.load(modelfile, weights_only=True)
             model.load_state_dict(tmp['state'])
             
 
